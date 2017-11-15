37997c7267f2cc2013e592a5d7e3744b265f5088
hanjianqiao <hanjianqiao@gmail.com>
2017-11-16 00:12:18 +0800

    update: category.js

---
+++ src/html/category.js
@@ -0,0 +1,20 @@
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+     results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+(function startLoad(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var obj = JSON.parse(this.responseText);
+
+    }
+  };
+  xhttp.open("GET", "playpath?video_id="+video_id, true);
+  xhttp.send();
+})();
